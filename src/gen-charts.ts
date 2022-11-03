spinozapps/PptxGenJS

feature: allow selecting round or square line cap on line charts

src/gen-charts.ts
@@ -19,7 +19,7 @@ import {
 	LETTERS,
 	ONEPT,
 } from './core-enums'
-import { IChartOptsLib, ISlideRelChart, ShadowProps, OptsChartData, IChartPropsTitle, OptsChartGridLine } from './core-interfaces'
+import { IChartOptsLib, ISlideRelChart, ShadowProps, OptsChartData, IChartPropsTitle, OptsChartGridLine, ChartLineCap } from './core-interfaces'
 import { createColorElement, genXmlColorSelection, convertRotationDegrees, encodeXmlEntities, getMix, getUuid, valToPts } from './gen-utils'
 import JSZip from 'jszip'
 
@@ -729,14 +729,14 @@ function makeChartType(chartType: CHART_NAME, data: OptsChartData[], opts: IChar
 					if (opts.lineSize === 0) {
 						strXml += '<a:ln><a:noFill/></a:ln>'
 					} else {
-						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="flat"><a:solidFill>' + createColorElement(seriesColor) + '</a:solidFill>'
+						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="' + createLineCap(opts.lineCap) + '"><a:solidFill>' + createColorElement(seriesColor) + '</a:solidFill>'
 						strXml += '<a:prstDash val="' + (opts.lineDash || 'solid') + '"/><a:round/></a:ln>'
 					}
 				} else if (opts.dataBorder) {
 					strXml +=
 						'<a:ln w="' +
 						valToPts(opts.dataBorder.pt) +
-						'" cap="flat"><a:solidFill>' +
+						'" cap="' + createLineCap(opts.lineCap) + '"><a:solidFill>' +
 						createColorElement(opts.dataBorder.color) +
 						'</a:solidFill><a:prstDash val="solid"/><a:round/></a:ln>'
 				}
@@ -995,7 +995,7 @@ function makeChartType(chartType: CHART_NAME, data: OptsChartData[], opts: IChar
 					if (opts.lineSize === 0) {
 						strXml += '<a:ln><a:noFill/></a:ln>'
 					} else {
-						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="flat"><a:solidFill>' + createColorElement(tmpSerColor) + '</a:solidFill>'
+						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="' + createLineCap(opts.lineCap) + '"><a:solidFill>' + createColorElement(tmpSerColor) + '</a:solidFill>'
 						strXml += '<a:prstDash val="' + (opts.lineDash || 'solid') + '"/><a:round/></a:ln>'
 					}
 
@@ -1978,4 +1978,17 @@ function createGridLineElement(glOpts: OptsChartGridLine): string {
 	strXml += '</c:majorGridlines>'
 
 	return strXml
+}
+
+function createLineCap(lineCap: ChartLineCap): string {
+	if (!lineCap || lineCap === 'flat') {
+		return 'flat';
+	} else if (lineCap === 'square') {
+		return 'sq';
+	} else if (lineCap === 'round') {
+		return 'rnd';
+	} else {
+		const neverLineCap: never = lineCap;
+		throw new Error(`Invalid chart line cap: ${neverLineCap}`);
+	}
 }

shbernal/ts-pptx

src/gen-charts.ts
@@ -19,7 +19,7 @@ import {
 	LETTERS,
 	ONEPT,
 } from './core-enums'
-import { IChartOptsLib, ISlideRelChart, ShadowProps, IChartPropsTitle, OptsChartGridLine, IOptsChartData } from './core-interfaces'
+import { IChartOptsLib, ISlideRelChart, ShadowProps, IChartPropsTitle, OptsChartGridLine, IOptsChartData, ChartLineCap } from './core-interfaces'
 import { createColorElement, genXmlColorSelection, convertRotationDegrees, encodeXmlEntities, getMix, getUuid, valToPts } from './gen-utils'
 import JSZip from 'jszip'
 
@@ -878,14 +878,14 @@ function makeChartType(chartType: CHART_NAME, data: IOptsChartData[], opts: ICha
 					if (opts.lineSize === 0) {
 						strXml += '<a:ln><a:noFill/></a:ln>'
 					} else {
-						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="flat"><a:solidFill>' + createColorElement(seriesColor) + '</a:solidFill>'
+						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="' + createLineCap(opts.lineCap) + '"><a:solidFill>' + createColorElement(seriesColor) + '</a:solidFill>'
 						strXml += '<a:prstDash val="' + (opts.lineDash || 'solid') + '"/><a:round/></a:ln>'
 					}
 				} else if (opts.dataBorder) {
 					strXml +=
 						'<a:ln w="' +
 						valToPts(opts.dataBorder.pt) +
-						'" cap="flat"><a:solidFill>' +
+						'" cap="' + createLineCap(opts.lineCap) + '"><a:solidFill>' +
 						createColorElement(opts.dataBorder.color) +
 						'</a:solidFill><a:prstDash val="solid"/><a:round/></a:ln>'
 				}
@@ -1132,7 +1132,7 @@ function makeChartType(chartType: CHART_NAME, data: IOptsChartData[], opts: ICha
 					if (opts.lineSize === 0) {
 						strXml += '<a:ln><a:noFill/></a:ln>'
 					} else {
-						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="flat"><a:solidFill>' + createColorElement(tmpSerColor) + '</a:solidFill>'
+						strXml += '<a:ln w="' + valToPts(opts.lineSize) + '" cap="' + createLineCap(opts.lineCap) + '"><a:solidFill>' + createColorElement(tmpSerColor) + '</a:solidFill>'
 						strXml += '<a:prstDash val="' + (opts.lineDash || 'solid') + '"/><a:round/></a:ln>'
 					}
 
@@ -2107,7 +2107,7 @@ function createShadowElement(options: ShadowProps, defaults: object): string {
 function createGridLineElement(glOpts: OptsChartGridLine): string {
 	let strXml: string = '<c:majorGridlines>'
 	strXml += ' <c:spPr>'
-	strXml += '  <a:ln w="' + valToPts(glOpts.size || DEF_CHART_GRIDLINE.size) + '" cap="flat">'
+	strXml += '  <a:ln w="' + valToPts(glOpts.size || DEF_CHART_GRIDLINE.size) + '" cap="' + createLineCap(glOpts.cap || DEF_CHART_GRIDLINE.cap) + '">'
 	strXml += '  <a:solidFill><a:srgbClr val="' + (glOpts.color || DEF_CHART_GRIDLINE.color) + '"/></a:solidFill>' // should accept scheme colors as implemented in [Pull #135]
 	strXml += '   <a:prstDash val="' + (glOpts.style || DEF_CHART_GRIDLINE.style) + '"/><a:round/>'
 	strXml += '  </a:ln>'
@@ -2115,4 +2115,17 @@ function createGridLineElement(glOpts: OptsChartGridLine): string {
 	strXml += '</c:majorGridlines>'
 
 	return strXml
+}
+
+function createLineCap(lineCap: ChartLineCap): string {
+	if (!lineCap || lineCap === 'flat') {
+		return 'flat';
+	} else if (lineCap === 'square') {
+		return 'sq';
+	} else if (lineCap === 'round') {
+		return 'rnd';
+	} else {
+		const neverLineCap: never = lineCap;
+		throw new Error(`Invalid chart line cap: ${neverLineCap}`);
+	}
 }